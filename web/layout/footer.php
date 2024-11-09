6a8a82d0600f47141c223ccb0b69604e041fbe0b
hotsaleprom <tiquetetrabajo@gmail.com>
2024-11-09 04:16:24 -0500

    Add files via upload

---
+++ web/layout/footer.php
@@ -0,0 +1,60 @@
+ <!-- FOOTER -->
+    <footer class="footer">
+        <div class="footer--main">
+            <div class="footer--logo">
+                <img src="./assets/logos/avianca_negative.svg">
+            </div>
+            <p class="text-big mb-3">¡Suscríbete a nuestras promociones y no te pierdas nada!</p>
+            <input class="input-text-secondary -i-mail mb-1" placeholder="Ej: nombre@email.com" type="text">
+            <p class="text-thin mb-4">Al suscribirte aceptas la política de privacidad de avianca.</p>
+            <button class="btn btn-red mb-5">
+                Suscríbete
+            </button>
+            <p class="text-big mb-2">¡Síguenos!</p>
+            <img class="mb-5" width="200px" src="./assets/svg/social_media_icons.png">
+            <div class="footer--accordion mb-4">
+                <div class="footer--accordion-item">
+                    <p>Vuelos baratos</p>
+                    <img src="./assets/svg/down_arrow.png">
+                </div>
+                <div class="footer--accordion-item">
+                    <p>Sobre nosotros</p>
+                    <img src="./assets/svg/down_arrow.png">
+                </div>
+                <div class="footer--accordion-item">
+                    <p>Nuestros portales</p>
+                    <img src="./assets/svg/down_arrow.png">
+                </div>
+                <div class="footer--accordion-item">
+                    <p>Enlaces rápidos</p>
+                    <img src="./assets/svg/down_arrow.png">
+                </div>
+            </div>
+        </div>
+
+        <hr width="99%">
+
+        <div class="footer--end">
+            <p>Copyright © Avianca 2024</p>
+            <img src="./assets/svg/Powered_by.png" width="130px">
+        </div>
+    </footer>
+
+
+
+
+<script src="./js/jquery-3.7.1.min.js"></script>
+<script src="./js/data.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+<script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/l10n/es.js"></script> 
+<script src="./js/moment.js"></script> 
+<script src="./js/calendarios.js"></script>
+
+<script src="./js/modal.js"></script>
+<script src="./js/desde.js"></script>
+<script src="./js/hacia.js"></script>
+<script src="./js/booking.js"></script>
+
+</body>
+
+</html>
---
+++ web/layout/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Avianca - Find cheap tickets and flights</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="shortcut icon" href="assets/favicon.png" t  ype="image/x-icon">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&amp;family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="layout/css/calendar.css">
+    <link rel="stylesheet" href="layout/css/main.css">
+    <link rel="stylesheet" href="layout/css/normalize.css">
+
+    <!-- START -->
+    <script src="js/functions.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="media/icon.ico"  />
+
+    <link rel="stylesheet" href="./css/style.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+
+    <title>Avianca | #NACIONALES</title>
+</head>
+
+<script>
+        // Deshabilitar clic derecho
+        document.addEventListener('contextmenu', event => event.preventDefault());
+
+        // Deshabilitar F12
+        document.addEventListener('keydown', function(event) {
+            if (event.keyCode == 123) { // F12
+                event.preventDefault();
+            } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Ctrl+Shift+I                
+                event.preventDefault();
+            } else if (event.ctrlKey && event.shiftKey && event.keyCode == 67) { // Ctrl+Shift+C
+                event.preventDefault();
+            } else if (event.ctrlKey && event.shiftKey && event.keyCode == 74) { // Ctrl+Shift+J
+                event.preventDefault();
+            } else if (event.ctrlKey && event.keyCode == 85) { // Ctrl+U
+                event.preventDefault();
+            }
+        }, false);
+</script>
+
+<body>
+    <nav class="navbar">
+        <img class="navbar-hmenu" src="assets/svg/hamburger_menu.png" alt="menu_h">
+        <div class="navbar--logo">
+            <img src="assets/logos/avianca_full.svg" alt="menu_h">
+        </div>
+    </nav>
+
